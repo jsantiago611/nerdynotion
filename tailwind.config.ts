206999f22205757570bd9a06a78f1d2f8d9d423c
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2025-02-06 13:40:55 -0500

    Replacing untitled with Martina

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,9 +23,7 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	corePlugins: {
-		// disable aspect ratio as per docs -> @tailwindcss/aspect-ratio
 		aspectRatio: false,
-		// disable some core plugins as they are included in the CSS even when unused
 		touchAction: false,
 		ringOffsetWidth: false,
 		ringOffsetColor: false,
@@ -52,7 +50,6 @@ export default {
 					"txt-dark": "#9B9B9B",
 					"bg-light": "#F1F1EF",
 					"bg-dark": "#2F2F2F", // this is Notion's choice
-					// 'bg-dark': '#566670', // this is my choice,
 					"bg-tag-light": "#E3E2E0",
 					"bg-tag-dark": "#5A5A5A",
 					"table-header-bg-light": "#F7F6F3",
@@ -69,7 +66,6 @@ export default {
 					"txt-dark": "#BA856F",
 					"bg-light": "#F4EEEE",
 					"bg-dark": "#4A3228",
-					// 'bg-dark': '#514E4B',
 					"bg-tag-light": "#EEE0DA",
 					"bg-tag-dark": "#603B2C",
 				},
@@ -78,7 +74,6 @@ export default {
 					"txt-dark": "#C77D48",
 					"bg-light": "#FBECDD",
 					"bg-dark": "#5C3B23",
-					// 'bg-dark': '#70544A',
 					"bg-tag-light": "#FADEC9",
 					"bg-tag-dark": "#854C1D",
 				},
@@ -87,7 +82,6 @@ export default {
 					"txt-dark": "#CA9849",
 					"bg-light": "#FBEDD6",
 					"bg-dark": "#56452F",
-					// 'bg-dark': '#707049',
 					"bg-tag-light": "#F9E4BC",
 					"bg-tag-dark": "#835E33",
 				},
@@ -96,7 +90,6 @@ export default {
 					"txt-dark": "#529E72",
 					"bg-light": "#EDF3EC",
 					"bg-dark": "#243D30",
-					// 'bg-dark': '#41706A',
 					"bg-tag-light": "#DBEDDB",
 					"bg-tag-dark": "#2B593F",
 				},
@@ -105,7 +98,6 @@ export default {
 					"txt-dark": "#5E87C9",
 					"bg-light": "#E7F3F8",
 					"bg-dark": "#143A4E",
-					// 'bg-dark': '#41727A',
 					"bg-tag-light": "#D3E5EF",
 					"bg-tag-dark": "#28456C",
 				},
@@ -114,7 +106,6 @@ export default {
 					"txt-dark": "#9D68D3",
 					"bg-light": "#F7F3F8",
 					"bg-dark": "#3C2D49",
-					// 'bg-dark': '#5A5672',
 					"bg-tag-light": "#E8DEEE",
 					"bg-tag-dark": "#492F64",
 				},
@@ -123,7 +114,6 @@ export default {
 					"txt-dark": "#9D68D3",
 					"bg-light": "#FBF2F5",
 					"bg-dark": "#4E2C3C",
-					// 'bg-dark': '#6A4C64',
 					"bg-tag-light": "#F5E0E9",
 					"bg-tag-dark": "#69314C",
 				},
@@ -132,17 +122,15 @@ export default {
 					"txt-dark": "#DF5452",
 					"bg-light": "#FDEBEC",
 					"bg-dark": "#522E2A",
-					// 'bg-dark': '#6A4545',
 					"bg-tag-light": "#FFE2DD",
 					"bg-tag-dark": "#6E3630",
 				},
 			},
 			fontFamily: {
-				// Set Untitled Sans as the sans font
-				sans: ['"Untitled Sans"', ...fontFamilySans],
-				serif: fontFamilySerif,
-				// Set Dinamit Bold as the mono font
-				mono: ['"Untitled Sans"', ...fontFamilyMono],
+				// Set Martina as the main font for all font families
+				sans: ['"Martina"', ...fontFamilySans],
+				serif: ['"Martina"', ...fontFamilySerif],
+				mono: ['"Martina"', ...fontFamilyMono],
 			},
 			transitionProperty: {
 				height: "height",
@@ -150,7 +138,6 @@ export default {
 		},
 	},
 	plugins: [
-		// require("@tailwindcss/typography"),
 		require("@tailwindcss/aspect-ratio"),
 		plugin(function ({ addComponents }) {
 			addComponents({
@@ -162,82 +149,39 @@ export default {
 				},
 			});
 		}),
-		// Use Tailwind's addBase to include the @font-face rules for our custom fonts.
+		// Add base styles for Martina font variants.
 		plugin(function ({ addBase }) {
 			addBase([
-				// Dinamit font variants
 				{
 					"@font-face": {
-						fontFamily: "Dinamit",
-						src: 'url(/fonts/Dinamit_Bold.woff2) format("woff2")',
-						// This variant is used as the mono font.
-						fontWeight: "700",
-						fontStyle: "normal",
-					},
-				},
-				{
-					"@font-face": {
-						fontFamily: "Dinamit",
-						src: 'url(/fonts/Dinamit_Regular.woff2) format("woff2")',
+						fontFamily: "Martina",
+						src: 'url(/fonts/MartinaPlantijn-Light.woff) format("woff")',
 						fontWeight: "400",
 						fontStyle: "normal",
 					},
 				},
 				{
 					"@font-face": {
-						fontFamily: "Dinamit",
-						src: 'url(/fonts/Dinamit_Extralight.woff2) format("woff2")',
-						fontWeight: "200",
-						fontStyle: "normal",
-					},
-				},
-				// Untitled Sans font variants
-				{
-					"@font-face": {
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-Light.woff2) format("woff2")',
-						fontWeight: "300",
-						fontStyle: "normal",
-					},
-				},
-				{
-					"@font-face": {
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-Regular.woff2) format("woff2")',
-						fontWeight: "400",
-						fontStyle: "normal",
-					},
-				},
-				{
-					"@font-face": {
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-RegularItalic.woff2) format("woff2")',
+						fontFamily: "Martina",
+						src: 'url(/fonts/MartinaPlantijn-LightItalic.woff) format("woff")',
 						fontWeight: "400",
 						fontStyle: "italic",
 					},
 				},
 				{
 					"@font-face": {
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-Medium.woff2) format("woff2")',
-						fontWeight: "500",
-						fontStyle: "normal",
-					},
-				},
-				{
-					"@font-face": {
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-Bold.woff2) format("woff2")',
+						fontFamily: "Martina",
+						src: 'url(/fonts/MartinaPlantijn-Black.woff) format("woff")',
 						fontWeight: "700",
 						fontStyle: "normal",
 					},
 				},
 				{
-					"@font-face": { 
-						fontFamily: "Untitled Sans",
-						src: 'url(/fonts/UntitledSansWeb-Black.woff2) format("woff2")',
-						fontWeight: "900",
-						fontStyle: "normal",
+					"@font-face": {
+						fontFamily: "Martina",
+						src: 'url(/fonts/MartinaPlantijn-BlackItalic.woff) format("woff")',
+						fontWeight: "700",
+						fontStyle: "italic",
 					},
 				},
 			]);
